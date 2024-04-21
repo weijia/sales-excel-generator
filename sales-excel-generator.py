da792679fa9389130d8a661ce07561fe3858c172
Richard <richardwangwang@gmail.com>
2024-04-21 18:04:09 +0800

    Update to use openpyxl.

--- sales-excel-generator.py
+++ sales-excel-generator.py
@@ -1,2 +1,16 @@
+from openpyxl import Workbook
+from openpyxl import load_workbook
+
+
+def main():
+    workbook = load_workbook(filename="import.xlsx")
+    workbook.sheetnames
+    sheet = workbook.active
+    print(sheet["A1"].value)
+    # for row in sheet:
+    #     print("%s: %s" % (row[0], row[1]))
+    #     break
+
+
 if __name__ == "__main__":
-    print("Hello world")
+    main()
